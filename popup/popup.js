4cc84782a543778f662389e03b60ecca67207fa8
Abin John <abinjohnanto@gmail.com>
2022-07-26 16:41:48 +0530

    update popup UI

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,11 @@
 'use strict';
 
-const timeEl = document.getElementById('time');
+const timeContainer = document.getElementById('time-container');
 chrome.runtime.sendMessage({ request: 'timeStat' }, (res) => {
-  console.log(res.timeStat);
-  timeEl.textContent = secondsToTime(res.timeStat);
+  setTime(res.timeStat);
 });
 
-const secondsToTime = (sec) => {
+const setTime = (sec) => {
   let hours = Math.floor(sec / 3600); // get hours
   let minutes = Math.floor((sec - hours * 3600) / 60); // get minutes
   let seconds = sec - hours * 3600 - minutes * 60; //  get seconds
@@ -15,7 +14,11 @@ const secondsToTime = (sec) => {
     (time) => (time < 10 ? '0' + time : '' + time) //Add 0 at the start if time is less than 10
   );
 
-  return hours === '00'
-    ? minutes + ':' + seconds //Return MM:SS if HH is 00
-    : hours + ':' + minutes + ':' + seconds; // Return in HH:MM:SS
+  let time = document.createElement('div');
+  time.innerHTML =
+    hours === '00'
+      ? `<span class="time">${minutes}</span><span class="time-identifier">m</span> <span class="time">${seconds}</span><span class="time-identifier">s</span>` //Return MM:SS if HH is 00
+      : `<span class="time">${hours}</span><span class="time-identifier">h</span> <span class="time">${minutes}</span><span class="time-identifier">m</span>`; // Return in HH:MM:SS
+
+  timeContainer.append(time);
 };
